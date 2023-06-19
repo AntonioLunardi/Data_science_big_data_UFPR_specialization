6d4e4d3f4fc2f91dfbb0103e6b7fbabc2a2c17bb
Antonio Lunardi <antonio.lunardi1@gmail.com>
2023-06-18 21:54:21 -0300

    Arquivo de como utilizar comandos do git adicionado

---
+++ Infrastructure/exercicio_versionamento.sh
@@ -0,0 +1,14 @@
+"""
+ Crie um repositório no GitHub. E faça alguns commits.
+ O projeto pode ser qualquer coisa. Pode também ser o projeto que foi feito na aula de hoje.
+ Compartilhe o projeto com o professor no GitHub.
+ Ou deixe o projeto público, e envie o link do repositório na tarefa.
+ Submeta o nome do projeto, e os envolvidos em um .txt no Moodle.
+ Se o projeto possui mais de uma pessoa, todos devem submeter o mesmo .txt no Moodle.
+
+ O usuário paulorla deve ter permissão para acessar o projeto e clonar na máquina local, 
+ para fazer a correção (ver os logs de quem fez
+ commit, e o quê).
+"""
+
+
